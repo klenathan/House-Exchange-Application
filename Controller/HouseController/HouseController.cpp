c69b6fdf4d2ae9c7aaeaea0a251145f2ab2fd636
Nam Thai Tran <tran.nam.thai.02@gmail.com>
2022-12-08 21:13:27 +0700

    [FEAT] :fire: added string constructor for CustomDate

--- Controller/HouseController/HouseController.cpp
+++ Controller/HouseController/HouseController.cpp
@@ -12,7 +12,8 @@ void HouseController::loadDataToArray() {
     vector<vector<string>> rawData = DataLoader::loadFile("../Data/house_data.csv");
 
     CustomDate startTestDate = CustomDate(10, 12, 2023);
-    CustomDate endTestDate = CustomDate(10, 12, 2023);
+    CustomDate endTestDate = CustomDate("12/02/2023");
+
     for (vector<string> line: rawData) {
         House temp_house = House(line[0], line[1],
                                  line[3], line[2], 0,
--- Model/CustomDate/CustomDate.cpp
+++ Model/CustomDate/CustomDate.cpp
@@ -3,8 +3,12 @@
 //
 
 #include <iostream>
-#include "CustomDate.h"
+#include <sstream>
+#include <vector>
 
+#include <exception>
+
+#include "CustomDate.h"
 
 
 /**
@@ -17,6 +21,87 @@
  *  412: INVALID_YEAR: Year is lower than 2022 (Present year)
  *
  * */
+
+CustomDate::CustomDate(int day, int month, int year) {
+    try {
+        ///// Day check
+        if (day > 31 || day < 1) {
+            throw 410;
+        }
+        ///// Month check with days
+        if (month > 12 || month < 1) {
+            throw 411;
+        } else {
+            if (!month % 2) {
+                //// Even Months
+                if (month == 2 && day > 29) throw 410;
+                else if (month < 8 && day > 30) throw 410;
+            } else {
+                //// Odd months
+                if (month > 8 && day > 30) throw 410;
+            }
+        }
+        //// Year check
+        if (year < 2022) {
+            throw 412;
+        }
+        this->day = day;
+        this->month = month;
+        this->year = year;
+    } catch (int e) {
+        std::cout << e;
+    }
+}
+
+/**
+ * @param: inputString has to follow the format of ("dd/mm/yyyy")
+ * */
+CustomDate::CustomDate(std::string inputString) {
+    //// turn string input into int for further processing
+    std::stringstream ss(inputString);
+    std::string text;
+
+    std::vector<int> tempArr;
+    while (std::getline(ss, text, '/')) {
+        try { tempArr.push_back(stoi(text)); }
+        catch (std::exception &e) {
+            std::cout << e.what() << std::endl;
+        }
+    }
+
+    //// Repeat the default constructor
+    int day = tempArr.at(0);
+    int month = tempArr.at(1);
+    int year = tempArr.at(2);
+    try {
+        ///// Day check
+        if (day > 31 || day < 1) {
+            throw 410;
+        }
+        ///// Month check with days
+        if (month > 12 || month < 1) {
+            throw 411;
+        } else {
+            if (!month % 2) {
+                //// Even Months
+                if (month == 2 && day > 29) throw 410;
+                else if (month < 8 && day > 30) throw 410;
+            } else {
+                //// Odd months
+                if (month > 8 && day > 30) throw 410;
+            }
+        }
+        //// Year check
+        if (year < 2022) {
+            throw 412;
+        }
+        this->day = day;
+        this->month = month;
+        this->year = year;
+    } catch (int e) {
+        std::cout << e;
+    }
+};
 
 std::string CustomDate::getDate() {
     return std::to_string(day) + "/" + std::to_string(month) + "/" + std::to_string(year) + "/";
