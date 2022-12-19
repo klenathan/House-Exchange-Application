d9a2faf4bbaf32298ce6b2e9d7c440f3563c7405
Nam Thai Tran <tran.nam.thai.02@gmail.com>
2022-12-19 14:09:53 +0700

    [MERGE] merge from Dong

--- Controller/RatingController/RatingController.h
+++ Controller/RatingController/RatingController.h
@@ -7,7 +7,7 @@
 
 #include <vector>
 #include <map>
-#include <bits/stdc++.h>
+//#include <bits/stdc++.h>
 #include "../../Data/DataLoader/DataHandler.h"
 #include "../../Model/HouseRating/HouseRating.h"
 #include "../../Model/UserRating/UserRating.h"
